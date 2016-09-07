58f53babf52b8700aff89b0e4e5a0b3f1d8a7125
unnoon <r.geertzema@gmail.com>
2016-09-06 23:56:54 -0600

    - added attribute parsing - added init, decorate, links, properties methods - saved old implementation to other files - lots of fiddling

--- test/prototypes/syntax.js
+++ test/prototypes/syntax.js
@@ -2,7 +2,7 @@
  * Created by Rogier on 26/08/2016.
  */
 
-const dsc = Symbol.for('dsc');
+const dsc = Symbol.for('cell-type.dsc');
 
 // let desc     = Object.getOwnPropertyDescriptor(properties, prop);
 // let attrs    = prop.match(/[\w\$\@]+/g); prop = attrs.pop();
@@ -22,16 +22,16 @@ var props = {
     // - not 100% fail safe if you define an object with attr$ property (although unlikely)
     // - getters and setters might mot need the attr$ prop per se
     // - functions might be identified as properties
-    name1: {attr$: "static frozen !enumerable override aliases=alias1|alias2", value: 666},  // using a special attr$ object to identify that we are dealing with an descriptor. not 100% fail safe if people also have objects with the name attr$ (although unlikely!)
+    name1: {attr$: "static frozen !enumerable override alias=alias1|alias2", value: 666},  // using a special attr$ object to identify that we are dealing with an descriptor. not 100% fail safe if people also have objects with the name attr$ (although unlikely!)
     // use a descriptor with symbol identification
     // + works with everything
-    // - still not 100% fail safe if other people would define the same global symbol
+    // - still not 100% fail safe if other people would define the same global symbol and is a bit more verbose as you'll prolly want a short ref to the symbol.
     // - functions might be identified as properties
     // - information might get lost in case we need to multiple times add a property
-    name2: {[dsc]: "static frozen !enumerable override aliases=alias1|alias2", value: 666},
+    name2: {[dsc]: "static frozen !enumerable override alias=alias1|alias2", value: 666},
     // use inline annotations + dsc for properties
-    // + neat, only verbose for properties that need extra attributes
-    name3: function() { "[@@dsc]: static frozen !enumerable override  aliases=alias1|alias   aliases=alias1|alias attr1";},
+    // + neat, only verbose for properties that need extra attributes. Which can be avoided by special methods like statics()
+    name3: function() { "[@@dsc]: static frozen !enumerable override  alias=alias1|alias2";},
     name4: {[dsc]: "static frozen !enumerable override aliases='alias1 alias2'", value: 666},
     // but could be used in combination with a dsc as above for properties
     // getters & setters
@@ -46,11 +46,23 @@ var props = {
 
 var attrs2 = "  static frozen !enumerable override  aliases=alias1|alias2|alias3   aliases2  =   alias4 |  alias5   |  alias6   attr1 ";
 
-attrs2 = attrs2.replace(/[\s]*([=\|\s])[\s]*/g, '$1'); // remove redundant white spaces
-console.log('matches: ', attrs2.match(/[\w\$!]+(=\w+(\|\w+)*)?/g)); // filter attributes
+attrs2 = attrs2.replace(/[\s]*([=\|\s])[\s]*/g, '$1'); // remove redundant white spaces. prettify
+console.log('matches: ', attrs2.match(/[\w\$!]+(=\w+(\|\w+)*)?/g)); // filter attributes including values
 
 syms = Object.getOwnPropertySymbols(props.name2);
 
 console.log(props.name2[dsc]);
-console.log(props.name2[Symbol.for('dsc')]);
+console.log(props.name2[Symbol.for('cell-type.dsc')]);
 console.log(syms);
+
+// out should be a plain js object
+// traits will not be allowed to have a linked object. This to avoid problems with inherited properties and super usage
+// Can a trait be a type????
+// Are there any functions that need to be wrapped? Static properties
+const TKiller = new Type('T-Killer').links(Cell).traits(T1, T2, T3).statics({}).properties({}).out; // out is used to output the actual object
+
+new Type('T-Killer').links(Cell).traits(T1, T2, T3).statics({}).properties({}).out;
+Type('T-Killer').links(Cell).traits(T1, T2, T3).statics({}).properties({}).out;
+Object.create(Type.prototype).init('T-Killer').links(Cell).traits(T1, T2, T3).statics({}).properties({}).out;
+Type('T-Killer', {links: Cell}).traits(T1, T2, T3).properties({}).out;
+Type('T-Killer', {links: Cell, traits: [T4, T5]}).properties({}).out;
